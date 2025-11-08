6c8e789939a700e7db70d271045eef80d29ade0c
EspaderaRio <espaderario@gmail.com>
2025-11-08 06:19:00 -0500

    Create control.js

---
+++ utils/control.js
@@ -0,0 +1,4 @@
+// ===================== CONTROL MODAL ===================== //
+
+openMenuTab.onclick = () => openModal(document.getElementById("MenuTab"));
+closeMenuTab.onclick = () => closeModal(document.getElementById("MenuTab"));
